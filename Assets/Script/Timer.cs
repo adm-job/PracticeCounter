cfd20f4246286bba3c3b02edf534b62f3555ae16
Stanislav <adm.job@yandex.ru>
2025-10-06 01:59:09 +0500

    init

--- Assets/Script/Timer.cs
+++ Assets/Script/Timer.cs
@@ -1,22 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Timer : MonoBehaviour
 {
-    [SerializeField]private TextMeshProUGUI _textMeshPro;
+    [SerializeField]private TextMeshProUGUI _textTimer;
     
-    private float _dealy;
+    private float _dealy = 0.5f;
+    private float _startTikmer = 0;
+
+    private void Start()
+    {
+        _textTimer.text = _startTikmer.ToString();
+    }
 
     private void Update()
     {
-        
+        if (Input.GetMouseButtonDown(1))
+        {
+            PrintTimer();
+        }
     }
 
     private IEnumerator PrintTimer()
     {
-        _textMeshPro.text = "";
+        _textTimer.text = "12334";
         yield return null;
 
     }
